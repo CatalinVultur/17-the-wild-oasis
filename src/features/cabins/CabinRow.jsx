03e4128b69eaa4fc1966ce2d1accfb04d71c0f85
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-14 15:55:04 +0300

    - Added delete cabin functionality, toast alerts

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,8 @@
 import { formatCurrency } from "../../utils/helpers.js";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins.js";
 import styled from "styled-components";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
     display: grid;
@@ -41,7 +44,20 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-    const { name, maxCapacity, regularPrice, discount, image } = cabin;
+    const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin;
+
+    const queryClient = useQueryClient(); // gives access to query client
+
+    const { isLoading: isDeleting, mutate } = useMutation({
+        mutationFn: (id) => deleteCabin(id),
+        onSuccess: () => {
+            toast.success('Cabin successfully deleted');
+            queryClient.invalidateQueries({ //automatically invalidate queries after deletion, forcing re-fetching
+                queryKey: ['cabins']
+            });
+        },
+        onError: (err) => toast.error(err.message)
+    });
 
     return (
         <TableRow role='row'>
@@ -54,7 +70,9 @@ function CabinRow({ cabin }) {
             <Discount>
                 {formatCurrency(discount)}
             </Discount>
-            <button>Delete</button>
+            <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+                Delete
+            </button>
         </TableRow>
     );
 }
