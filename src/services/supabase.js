d4916a10a4fa89467fb34007a382e99e08421da5
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-17 15:25:25 +0300

    - added validation & image upload to cabin creation

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zqifkmzldfinyjilcxjo.supabase.co'
+export const supabaseUrl = 'https://zqifkmzldfinyjilcxjo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxaWZrbXpsZGZpbnlqaWxjeGpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyODM3MTQsImV4cCI6MjAzMzg1OTcxNH0.ddT4aKPHIX9-7MBUW4LWXvgmyopD7Pkq4Pjwrz2vI3k';
 
 const supabase = createClient(supabaseUrl, supabaseKey)
