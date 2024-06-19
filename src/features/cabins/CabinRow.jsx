f39aaba6435c9a1b851f808a809b7d5e4148c7bc
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-19 16:32:33 +0300

    - refactored table to use compound component pattern

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,18 +7,7 @@ import CreateCabinForm from "./CreateCabinForm.jsx";
 import {HiPencil, HiSquare2Stack, HiTrash} from "react-icons/hi2";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
-
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+import Table from "../../ui/Table.jsx";
 
 const Img = styled.img`
     display: block;
@@ -72,7 +61,7 @@ function CabinRow({cabin}) {
     }
 
     return (
-        <TableRow role='row'>
+        <Table.Row columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
             <Img src={image}/>
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -102,13 +91,13 @@ function CabinRow({cabin}) {
                         <CreateCabinForm cabinToEdit={cabin} />
                     </Modal.Window>
 
-                    <Modal.Open>
+                    <Modal.Open opens='delete'>
                         <button>
                             <HiTrash/>
                         </button>
                     </Modal.Open>
 
-                    <Modal.Window>
+                    <Modal.Window name='delete'>
                         <ConfirmDelete
                             resourceName='cabin'
                             disabled={isDeleting}
@@ -117,7 +106,7 @@ function CabinRow({cabin}) {
                     </Modal.Window>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
