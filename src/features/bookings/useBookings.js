b2f230cc4aea6bbd4507518a3bf01a03276d9abb
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-20 15:50:35 +0300

    - added filters to bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,17 @@
+import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings.js";
 
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+    // Filter
+    const filterValue = searchParams.get('status');
+    const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue }
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings
+        queryKey: ['bookings', filter], // filter - refreshes data when changed
+        queryFn: () => getBookings({ filter })
     });
 
     return {
