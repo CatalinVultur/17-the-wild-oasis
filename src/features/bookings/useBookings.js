88d59ed61de8891e120fc20e9f5f589e4780de22
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-20 15:58:50 +0300

    - added sort functionality to bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -9,9 +9,14 @@ export function useBookings() {
     const filterValue = searchParams.get('status');
     const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue }
 
+    // Sort
+    const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+    const [field, direction] = sortByRaw.split('-');
+    const sortBy = { field, direction };
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ['bookings', filter], // filter - refreshes data when changed
-        queryFn: () => getBookings({ filter })
+        queryKey: ['bookings', filter, sortBy], // filter, sortBy - refreshes data when changed
+        queryFn: () => getBookings({ filter, sortBy })
     });
 
     return {
