0a0d7183435cea301abe62eeb0d4c43565c5810a
CatalinVultur <catalin.vultur@amdaris.com>
2024-06-21 14:08:35 +0300

    - added prefetching to pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
 import { useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings.js";
+import { PAGE_SIZE } from '../../utils/constants.js';
 
 export function useBookings() {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
 
     // Filter
@@ -17,10 +19,30 @@ export function useBookings() {
     // Pagination
     const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+    // Query
     const { isLoading, data: { data: bookings, count } = {} , error } = useQuery({
         queryKey: ['bookings', filter, sortBy, page], // filter, sortBy - refreshes data when changed
         queryFn: () => getBookings({ filter, sortBy, page })
     });
+
+    // Prefetching
+    const pageCount = Math.ceil(count / PAGE_SIZE);
+
+    // next page
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+        });
+    }
+
+    // prev page
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page - 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page - 1 })
+        });
+    }
 
     return {
         isLoading,
